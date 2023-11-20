9ccafe2cc87dbf78671b2c7f216f7ea52641bfb6
sushant <sushantlama732@gmail.com>
2023-11-20 09:59:06 +0545

    added dirname

--- index.js
+++ index.js
@@ -15,11 +15,6 @@ var port = process.env.PORT || 6000;
 //  process.env.URI 
 // const fileUpload = require('express-fileupload');
 
-app.use(express.static(__dirname));
-
-app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
 dotenv.config()
 const corsConfig = {
   origin: '',
